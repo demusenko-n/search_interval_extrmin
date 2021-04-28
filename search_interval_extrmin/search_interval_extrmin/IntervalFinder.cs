72064bf96c7df632386985d07160d0efa5282ea3
Демусенко Микита <demusenko02@gmail.com>
2021-04-28 17:16:59 +0300

    hotfixv1

--- search_interval_extrmin/search_interval_extrmin/IntervalFinder.cs
+++ search_interval_extrmin/search_interval_extrmin/IntervalFinder.cs
@@ -4,10 +4,25 @@ namespace IntervalHelper
 {
     public class IntervalFinder
     {
-        public (double, double) GetInterval(Func<double,double> function, double x0, double step)
-        {
+        private int GetSign(Func<double, double> function, double x0, double step)
+            => Math.Sign(function(x0) - function(x0 + step));
 
-            return (0, 0);
+
+        public (double, double) GetInterval(Func<double, double> function, double x0, double step)
+        {
+            int sign = GetSign(function, x0, step);
+
+            double xkPrev = x0;
+            double xk = xkPrev + step * sign;
+
+
+            while (function(xk) < function(xkPrev))
+            {
+                xkPrev = xk;
+                xk += step * sign;
+            }
+
+            return (xkPrev - step * sign, xk);
         }
     }
 }
