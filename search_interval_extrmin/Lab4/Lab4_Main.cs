fe0d3eb28f1cc212cd5fb596f2825465de8eb708
Демусенко Микита <demusenko02@gmail.com>
2021-04-28 17:01:12 +0300

    Added func

---
+++ search_interval_extrmin/Lab4/Lab4_Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IntervalHelper;
+
+namespace Lab4
+{
+    class Lab4Main
+    {
+        static double Function(double x)
+            => 1 - Math.Pow(1 - Math.Pow((x - 6) / 5, 4), 0.25);
+
+        static void Main(string[] args)
+        {
+            IntervalFinder intervalFinder = new();
+            
+            (double a, double b) = intervalFinder.GetInterval(Function, 2, 0.2);
+        }
+    }
+}
