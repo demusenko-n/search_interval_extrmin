26027bb3940c3f8841799c000e3f61cccc93d197
Демусенко Микита <demusenko02@gmail.com>
2021-04-28 17:01:00 +0300

    Changed class name

---
+++ search_interval_extrmin/search_interval_extrmin/IntervalFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntervalHelper
+{
+    public class IntervalFinder
+    {
+        public (double, double) GetInterval(Func<double,double> function, double x0, double step)
+        {
+
+            return (0, 0);
+        }
+    }
+}
